807b3af2050bb86663fa717b037d7a8441544865
Andrew <atanner2727@gmail.com>
2014-11-26 11:37:05 -0500

    started order js, added locations

---
+++ myproject/js/order.js
@@ -0,0 +1,8 @@
+function setOrder()
+{
+	document.cookie="smallqty=John Doe";
+	document.cookie="largeqty=John Doe";
+	document.cookie="donation=John Doe";
+	document.cookie="location=John Doe";
+}
+
